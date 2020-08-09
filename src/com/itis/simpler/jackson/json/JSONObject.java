782d089f36aef8444bb7a3be2fb88a9cde6b4445
Gbemiro Jiboye <gbenroscience@yahoo.com>
2020-08-10 00:04:41 +0100

    Added more convenience methods to class

--- src/com/itis/simpler/jackson/json/JSONObject.java
+++ src/com/itis/simpler/jackson/json/JSONObject.java
@@ -18,6 +18,7 @@ import java.math.BigInteger;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+
 /**
  *
  * @author GBEMIRO JIBOYE <gbenroscience@gmail.com>
@@ -41,7 +42,6 @@ public class JSONObject {
         }
     }
 
-    
     public void put(String key, String value) {
         parseNode.put(key, value); // building
     }
@@ -93,21 +93,12 @@ public class JSONObject {
         parseNode.set(key, value.parseNode);
     }
 
-    public void put(String key, Object value) {
-        parseNode.putPOJO(key, value);
+    public void put(String key, JSONArray value) {
+        parseNode.set(key, value.parseNode);
     }
 
-    public static class Parser<T> {
-
-        public T decode(String json, Class clazz) {
-            try {
-                return new Converter<T>().fromJsonString(json, clazz);
-            } catch (IOException ex) {
-                Logger.getLogger(JSONObject.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            return null;
-        }
-
+    public void put(String key, Object value) {
+        parseNode.putPOJO(key, value);
     }
 
     public int optInt(String key) {
@@ -118,12 +109,28 @@ public class JSONObject {
         return 0;
     }
 
+    public int optInt(String key, int def) {
+        if (parseNode != null) {
+            JsonNode nod = parseNode.get(key);
+            return nod != null ? nod.asInt(def) : def;
+        }
+        return def;
+    }
+
     public long optLong(String key) {
         if (parseNode != null) {
             JsonNode nod = parseNode.get(key);
             return nod != null ? nod.asLong(0) : 0;
         }
         return 0;
+    }
+
+    public long optLong(String key, long def) {
+        if (parseNode != null) {
+            JsonNode nod = parseNode.get(key);
+            return nod != null ? nod.asLong(def) : def;
+        }
+        return def;
     }
 
     public double optDouble(String key) {
@@ -134,12 +141,28 @@ public class JSONObject {
         return 0;
     }
 
+    public double optDouble(String key, double def) {
+        if (parseNode != null) {
+            JsonNode nod = parseNode.get(key);
+            return nod != null ? nod.asDouble(def) : def;
+        }
+        return def;
+    }
+
     public boolean optBoolean(String key) {
         if (parseNode != null) {
             JsonNode nod = parseNode.get(key);
             return nod != null ? nod.asBoolean(false) : false;
         }
         return false;
+    }
+
+    public boolean optBoolean(String key, boolean def) {
+        if (parseNode != null) {
+            JsonNode nod = parseNode.get(key);
+            return nod != null ? nod.asBoolean(def) : def;
+        }
+        return def;
     }
 
     public double optFloat(String key) {
@@ -150,6 +173,14 @@ public class JSONObject {
         return 0;
     }
 
+    public double optFloat(String key, float def) {
+        if (parseNode != null) {
+            JsonNode nod = parseNode.get(key);
+            return nod != null && nod.isFloat() ? nod.floatValue() : def;
+        }
+        return def;
+    }
+
     public short optShort(String key) {
         if (parseNode != null) {
             JsonNode nod = parseNode.get(key);
@@ -158,14 +189,48 @@ public class JSONObject {
         return 0;
     }
 
+    public short optShort(String key, short def) {
+        if (parseNode != null) {
+            JsonNode nod = parseNode.get(key);
+            return nod != null && nod.isShort() ? nod.shortValue() : def;
+        }
+        return def;
+    }
+
     public byte optByte(String key) {
         if (parseNode != null) {
             JsonNode nod = parseNode.get(key);
 
-            return nod != null && nod.isShort() ? (byte) nod.asInt(0) : 0;
+            if (nod != null && nod.isShort()) {
+
+                short val = (short) nod.asInt();
+                if (val >= -128 && val <= 127) {
+                    return (byte) val;
+                }
+
+            }
+            return 0;
 
         }
         return 0;
+    }
+
+    public byte optByte(String key, byte def) {
+        if (parseNode != null) {
+            JsonNode nod = parseNode.get(key);
+
+            if (nod != null && nod.isShort()) {
+
+                short val = (short) nod.asInt(def);
+                if (val >= -128 && val <= 127) {
+                    return (byte) val;
+                }
+
+            }
+            return def;
+
+        }
+        return def;
     }
 
     public JSONObject optJSONObject(String key) {
@@ -207,6 +272,25 @@ public class JSONObject {
         return "";
     }
 
+    public boolean has(String key) {
+        if (parseNode != null) {
+            return parseNode.has(key);
+        }
+        return false;
+    }
+
+    public HashSet<String> keySet() {
+        HashSet<String> set = new HashSet<String>();
+
+        Iterator<String> it = parseNode.fieldNames();
+
+        while (it.hasNext()) {
+            set.add(it.next());
+        }
+
+        return set;
+    }
+
     @Override
     public String toString() {
         return parseNode.toString();
@@ -216,6 +300,5 @@ public class JSONObject {
         return parseNode.toPrettyString();
     }
 
-
-
 }
+
